094bd80af48c7a93c5184fcbff57665de7ebd6d4
codemossaka <peyagodson@ymail.com>
2024-09-16 22:21:40 +0300

    first commit

--- app/src/main/java/ru/godsonpeya/bpmn/MainActivity.kt
+++ app/src/main/java/ru/godsonpeya/bpmn/MainActivity.kt
@@ -8,14 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import ru.godsonpeya.bpmn.ui.theme.BPMNTheme
+import ru.godsonpeya.bpmn.ui.theme.ColumnListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BPMNTheme {
+            ColumnListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ChantApp(modifier = Modifier.padding(innerPadding))
                 }
--- app/src/main/java/ru/godsonpeya/bpmn/ui/theme/Theme.kt
+++ app/src/main/java/ru/godsonpeya/bpmn/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package ru.godsonpeya.bpmn.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun BPMNTheme(
+fun ColumnListTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
